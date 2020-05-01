c7a47739c91c6e55d6ae3fa3e5dc003fd95bcfa0
CIRCERO <kevyn@axxess.co.za>
2020-05-01 08:24:34 +0200

    update repo

---
+++ create_repository.sh
@@ -0,0 +1,8 @@
+create_repository.py \
+        --datadir=~/appdata/roaming/kodi \
+        --compressed \
+        https://github.com/chadparry\
+/kodi-repository.chad.parry.org.git:repository.chad.parry.org \
+        https://github.com/chadparry\
+/kodi-plugin.program.remote.control.browser.git\
+:plugin.program.remote.control.browser
